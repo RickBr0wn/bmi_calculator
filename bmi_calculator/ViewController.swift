a5421c25b23a9bec45c41afead38a207ac42db05
Rick Brown <ricky.brown.00@gmail.com>
2020-10-28 23:58:40 +0000

    Initial Commit

---
+++ bmi_calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bmi_calculator
+//
+//  Created by Rick Brown on 28/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
